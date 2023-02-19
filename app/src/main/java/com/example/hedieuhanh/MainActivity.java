933347e42297649f4283b1e4c6a432722667c8af
tienmanh2k1 <lickvovan@gmail.com>
2023-02-20 00:32:15 +0700

    update

--- app/src/main/java/com/example/hedieuhanh/MainActivity.java
+++ app/src/main/java/com/example/hedieuhanh/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.hedieuhanh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    Button btnAdd;
+
+    List<String> stringList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.hdh_listview);
+        btnAdd = findViewById(R.id.btn_add);
+
+        for (int i = 0 ; i < 100 ; i++){
+            stringList.add("HDH : " + i);
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.activity_hdh,R.id.title,stringList);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
